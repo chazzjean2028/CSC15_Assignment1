0bc93395fcb5d344f9b82e89327eb0efd23ab0e7
chazzjean2028 <cjean9@pride.hofstra.edu>
2024-10-14 22:03:27 -0400

    Update beetle.py

--- beetle.py
+++ beetle.py
@@ -1,11 +1,28 @@
 '''
     This python program plays the beetle game
 
-    Author: ADD YOUR NAME HERE
-    Date:   ADD THE DATE COMPLETED HERE
+    Author: Chazz Jean
+    Date:   October 14, 2024
 
 '''
-
 import beetle_functions
+def add_part(body, dice_roll)
+    assert(dice_roll >=1 and dice_roll <=6)
+
+def dict_dice2body {
+    "body": "1",
+    "head": "2",
+    "legs": "3",
+    "eyes": "4",
+    "antennae": "5",
+    "tail": "6"}
+
+def roll_dice
+    print("player_name,"'s turn")
+    random.randint(1,6)
+    print(player_name, "rolled", dice_value)
+    print(dice_value)
+    
 beetle_functions.play_multiple_games()
+
 
